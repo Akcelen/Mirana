66162668fab616fcb34052c18d1d2322d5acf3aa
Akcelen <40669883+Akcelen@users.noreply.github.com>
2018-11-26 21:37:38 +0300

    Обновление

---
+++ browser.bat
@@ -0,0 +1,2 @@
+cd D:\WebProject\Portfolio\Mirana
+browser-sync start --server --files "php/*.php,css/*.css,js/*.js,html/*.html,*.*"
--- script/script.js
+++ script/script.js
@@ -1,46 +1,47 @@
-var slideIndex = 1;
-function slider() {
-	if (document.documentElement.clientWidth <= 768 ) {
-		showSlides(slideIndex);
-	}
-}
-function pluseSlides(n) {
-	showSlides(slideIndex += n);
-}
-
-function curentSlide(n) {
-	showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-	var i;
-	var slides = document.getElementsByClassName("Slides");
-	var dots = document.getElementsByClassName("dot");
-
-	if ( n > slides.length) {
-		slideIndex = 1
-	}
-	if ( n<1) {
-		slideIndex=slides.length
-	}
-	for (var i = 0; i < slides.length; i++) {
-		slides[i].style.display = "none";
-	}
-	for (var i = 0; i < dots.length; i++) {
-		dots[i].className = dots[i].className.replace("active","");
-	}
-	slides[slideIndex - 1].style.display = " block";
-	dots[slideIndex - 1].className += " active";
-}
 
 var uluru = {lat: 48.5264862, lng: 39.1499637};	    
-    function initMap() {
-    	var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 10, 
-          center: uluru
-        });
-        var marker = new google.maps.Marker({
-        	position: uluru,
-        	map: map
-     	})
-  }
+function initMap() {
+	var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 10, 
+      center: uluru
+    });
+    var marker = new google.maps.Marker({
+    	position: uluru,
+    	map: map
+ 	})
+ }
+ $(document).ready(function(){
+ 	$('.popup-btn').on('click',function(event) {
+ 		event.preventDefault();
+ 		$('.popup').fadeIn();
+ 	});
+ 	$('.popup-close').on('click',function(event) {
+ 		event.preventDefault();
+ 		$('.popup').fadeOut();
+ 	});
+	$('.recent').slick({
+	  	infinite: true,
+			slidesToShow: 4,
+			slidesToScroll: 1,
+			responsive: [
+			    {
+			      breakpoint: 1024,
+			      settings: {
+			        slidesToShow: 2,
+			        slidesToScroll: 1,
+			        infinite: true,
+			        prevArrow: '<button class="arrow prev"></button>',
+			        nextArrow: '<button class="arrow next"></button>'
+			      }
+			    },
+			    {
+			    breakpoint: 640,
+			      settings: {
+			        slidesToShow: 1,
+			        slidesToScroll: 1,
+			        infinite: true
+			    }
+			}
+		]
+	})
+});
